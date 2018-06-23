5817c02b162220d406a3a85e8e849746a0a52f41
betapl3b <38052075+betapl3b@users.noreply.github.com>
2018-06-23 16:29:14 +0300

    edited move_base params, fixed controller node
    changed local planner to DWAPlannerROS
    edited config files
    edited world files

--- project_simulation/scripts/go.py
+++ project_simulation/scripts/go.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import rospy
 from geometry_msgs.msg import PoseStamped
+from audio_common_msgs.msg import AudioData
 def goHome():
     global targetPose
     targetPose.pose.orientation.w = 0
@@ -8,33 +9,22 @@ def goHome():
     targetPose.pose.position.z = 0
     targetPose.pose.position.x = 4
     targetPose.pose.position.y = 5
-    targetPose.header.frame_id = 'world'
-    #
-    # if int(targetMarker) == 1:
-    #     targetPose.pose.position.x = 2.34
-    #     targetPose.pose.position.y = -9
-    # elif int(targetMarker) == 2:
-    #     targetPose.pose.position.x = 6
-    #     targetPose.pose.position.y = 9.5
-    # elif int(targetMarker) == 3:
-    #     targetPose.pose.position.x = 9
-    #     targetPose.pose.position.y = 8
-    # elif int(targetMarker) == 4:
-    #     targetPose.pose.position.x = 6
-    #     targetPose.pose.position.y = 0.84
-    # else:
-    #     targetPose = PoseStamped()
-    #     targetPose.pose.orientation.w = 1
+    targetPose.header.frame_id = 'map'
     coords.publish(targetPose)
 
+def audioCaptureCallback(audioArray):
+    global clapTest
+    clapTest = len(audioArray.data)
+
 def deliverobotino():
+    global clapTest
     goNext = 0
     while goNext == 0:
         global targetMarker
         targetMarker = int(raw_input('Who needs my help now? (1-4)\n'))
         targetPose.pose.orientation.w = 0.5
         targetPose.pose.orientation.z = 0.5
-        targetPose.pose.position.x = 1.66
+        targetPose.pose.position.x = 2
         targetPose.pose.position.z = -1
         targetPose.pose.position.y = -0.86
         if int(targetMarker) == 1:
@@ -42,12 +32,12 @@ def deliverobotino():
             goNext+=1
         elif int(targetMarker) == 2:
             targetPose.header.frame_id = 'ar_marker_2'
-            targetPose.pose.position.x = -1
+            targetPose.pose.position.x = -2
             targetPose.pose.position.z = -1
             goNext += 1
         elif int(targetMarker) == 3:
             targetPose.header.frame_id = 'ar_marker_4'
-            targetPose.pose.position.x = -1
+            targetPose.pose.position.x = -2
             targetPose.pose.position.z = -1
             goNext += 1
         elif int(targetMarker) == 4:
@@ -57,11 +47,13 @@ def deliverobotino():
             print('Wrong number')
     coords.publish(targetPose)
     print('Clap when I can go. \n')
-    clapTest = raw_input('Press any key to continue... \n')
+    while not clapTest > 500:
+        rospy.loginfo('Microphone volume= %d (Clap for 500 VOLUME to go home)', clapTest)
+    rospy.logwarn('I heard the clap! Going home now.')
     goHome()
 
-
 rospy.init_node('controller_node')
+rospy.Subscriber('/audio/audio', AudioData, audioCaptureCallback, queue_size=10)
 coords = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
 oneMore='test'
 while not oneMore == 'quit':
